a1c3d93e388b1615178d9619bbe1992c6d146f60
Aris Vangelis <avangelis@byte.gr>
2019-09-26 19:23:49 +0300

    404page

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,30 +1,41 @@
 const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
 
 const app = express()
 
-const publicDirectoryPath = path.join(__dirname, '../Public')
-app.use(express.static(publicDirectoryPath))
+//define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
 
+//setup handlebars engine and views location
 app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath);
+
+//setup static directory to serve
+app.use(express.static(publicDirectoryPath))
 
 app.get('', (req, res) => {
     res.render('index', {
-        title:'Weather app',
-        name: 'Aris Vangelis'
+        title: 'Weather app',
+        name: 'Created by Aris Vangelis'
     })
 })
 
-app.get('/about', (req,res)=>{
+app.get('/about', (req, res) => {
     res.render('about', {
-        title:'About me',
-        name:'Aris vangelis'
+        title: 'About me',
+        name: 'Created by Aris vangelis'
     })
 })
 
-app.get('/help', (req,res)=>{
+app.get('/help', (req, res) => {
     res.render('help', {
-        message :'Hello world'
+        message: 'Hello world',
+        title: 'Help',
+        name: 'Created by Aris vang'
     })
 })
 
@@ -32,7 +43,18 @@ app.get('/weather', (req, res) => {
     res.send('Weather')
 })
 
+app.get('/help/*', (req, res) => {
+    res.render('404page', {
+        message: 'Page not found'
+    })
+})
 
+app.get('/about/*', (req, res) => {
+    res.render('404page', {
+        message: 'Page not found',
+        name: 'Created by Aris vangelis'
+    })
+})
 
 app.listen(3000, () => {
     console.log('Server is up on port 3000')
